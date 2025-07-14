993b20e29dd4af0805a13f497dd07993c44f9b50
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 14:10:41 +0400

    Create AddButtonSkeleton.tsx

---
+++ frontend/src/components/loader/productdisplay/AddButtonSkeleton.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/addButton_skeleton.module.scss'
+
+function AddButtonSkeleton() {
+  return (
+    <div className={`addbutton_skeleton ${styles.addbutton_skeleton}`}></div>
+  )
+}
+
+export default AddButtonSkeleton
