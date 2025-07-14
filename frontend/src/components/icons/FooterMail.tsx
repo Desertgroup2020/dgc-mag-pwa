29df3e7533edb0f5be5ac584059c7c29dfcc7ef8
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 14:08:25 +0400

    Create FooterMail.tsx

---
+++ frontend/src/components/icons/FooterMail.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const FooterMailIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="1 12 25 1"
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#7E8B53"
+      d="M4 20.794c-.55 0-1.02-.196-1.412-.587A1.93 1.93 0 0 1 2 18.794v-12c0-.55.196-1.02.588-1.412A1.93 1.93 0 0 1 4 4.794h16c.55 0 1.021.196 1.413.588.392.392.588.863.587 1.412v12c0 .55-.196 1.02-.587 1.413a1.92 1.92 0 0 1-1.413.587H4Zm8-7-8-5v10h16v-10l-8 5Zm0-2 8-5H4l8 5Zm-8-3v-2 12-10Z"
+      opacity={0.84}
+    />
+  </svg>
+)
+export default FooterMailIcon
