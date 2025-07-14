bc4083b2932e9ecf9eb33770ca4534d3d8e06826
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 14:08:17 +0400

    Create FooterFacebook.tsx

---
+++ frontend/src/components/icons/FooterFacebook.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const FooterFacebookIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#939393"
+      d="M22 12.855c0-5.557-4.477-10.061-10-10.061S2 7.298 2 12.854c0 5.023 3.656 9.185 8.437 9.94v-7.03H7.9v-2.91h2.538v-2.216c0-2.521 1.493-3.915 3.778-3.915 1.093 0 2.238.197 2.238.197v2.476h-1.262c-1.242 0-1.628.776-1.628 1.572v1.887h2.773l-.443 2.908h-2.33v7.03C18.344 22.04 22 17.878 22 12.856Z"
+    />
+  </svg>
+)
+export default FooterFacebookIcon
