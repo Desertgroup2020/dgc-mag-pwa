a774445fc38de23e817e9efb1a656c3f9d4c2c7c
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 14:08:20 +0400

    Create FooterLocation.tsx

---
+++ frontend/src/components/icons/FooterLocation.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const FooterLocation = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="1 12 25 1"
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#7E8B53"
+      fillRule="evenodd"
+      d="M7.25 10.794a4.75 4.75 0 1 1 9.5 0 4.75 4.75 0 0 1-9.5 0ZM12 7.544a3.25 3.25 0 1 0 0 6.5 3.25 3.25 0 0 0 0-6.5Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#7E8B53"
+      fillRule="evenodd"
+      d="M3.524 9.65a8.288 8.288 0 0 1 8.26-7.606h.432a8.288 8.288 0 0 1 8.26 7.607 8.944 8.944 0 0 1-1.99 6.396l-4.793 5.86a2.186 2.186 0 0 1-3.386 0l-4.793-5.86a8.943 8.943 0 0 1-1.99-6.396Zm8.26-6.106a6.788 6.788 0 0 0-6.764 6.23 7.443 7.443 0 0 0 1.656 5.323l4.793 5.862a.686.686 0 0 0 1.064 0l4.793-5.862a7.443 7.443 0 0 0 1.654-5.323 6.788 6.788 0 0 0-6.765-6.23h-.43Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default FooterLocation
