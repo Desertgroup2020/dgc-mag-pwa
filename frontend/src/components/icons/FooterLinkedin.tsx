d45a5806fc741e70df496ee6eb7cbac1c645afd1
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 14:08:15 +0400

    Create FooterLinkedin.tsx

---
+++ frontend/src/components/icons/FooterLinkedin.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const FooterLinkedinIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#939393"
+      d="M21 5.117V20.47a1.323 1.323 0 0 1-1.323 1.324H4.324A1.324 1.324 0 0 1 3 20.47V5.117a1.324 1.324 0 0 1 1.324-1.323h15.353A1.323 1.323 0 0 1 21 5.117Zm-12.706 5.56H5.647v8.47h2.647v-8.47Zm.238-2.912a1.525 1.525 0 0 0-1.514-1.536h-.047a1.535 1.535 0 1 0 0 3.07 1.525 1.525 0 0 0 1.561-1.487v-.047ZM18.352 14c0-2.546-1.62-3.537-3.229-3.537a3.018 3.018 0 0 0-2.678 1.366h-.074v-1.154H9.882v8.47h2.647v-4.504a1.758 1.758 0 0 1 1.589-1.896h.1c.842 0 1.467.53 1.467 1.864v4.537h2.647l.02-5.146Z"
+    />
+  </svg>
+)
+export default FooterLinkedinIcon
