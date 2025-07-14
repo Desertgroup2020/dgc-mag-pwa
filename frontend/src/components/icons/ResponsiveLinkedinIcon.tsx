3e7628aecd8007b077ead2769849fd0a5500d63a
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 14:09:11 +0400

    Create ResponsiveLinkedinIcon.tsx

---
+++ frontend/src/components/icons/ResponsiveLinkedinIcon.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const ResponsiveLinkedIn = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={19}
+    height={20}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#939393"
+      d="M16.091 4.185v11.63a1.003 1.003 0 0 1-1.003 1.004H3.458a1.002 1.002 0 0 1-1.003-1.003V4.185a1.003 1.003 0 0 1 1.002-1.003h11.631a1.003 1.003 0 0 1 1.003 1.003ZM6.465 8.396H4.46v6.417h2.005V8.396Zm.18-2.206A1.155 1.155 0 0 0 5.5 5.027h-.036a1.163 1.163 0 0 0 0 2.326 1.155 1.155 0 0 0 1.183-1.127V6.19Zm7.44 4.725c0-1.93-1.227-2.68-2.446-2.68A2.286 2.286 0 0 0 9.61 9.27h-.055v-.874H7.668v6.417h2.006V11.4a1.331 1.331 0 0 1 1.203-1.436h.076c.638 0 1.111.401 1.111 1.412v3.437h2.006l.016-3.898Z"
+    />
+  </svg>
+)
+export default ResponsiveLinkedIn
