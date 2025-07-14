59f1e26b6972a02674fd6fb9fbff8c0a5b515b57
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 15:45:08 +0400

    Create ProductNotFound.tsx

---
+++ frontend/src/features/product-listing/components/ProductNotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ProductNotFound() {
+  return (
+    <div className="pr_not_found py-4">
+      <div className="container">
+        <p className="txt text-h1">Product Not Found!</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductNotFound;
