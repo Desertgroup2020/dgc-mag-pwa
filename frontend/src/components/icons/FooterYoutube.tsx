52519b0dbf6c11c5b47eac4ba625d46ece9df161
Desert Group <abdulquadir.shahid@desertgroup.ae>
2025-07-14 14:08:39 +0400

    Create FooterYoutube.tsx

---
+++ frontend/src/components/icons/FooterYoutube.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const FooterYoutubeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#939393"
+      fillRule="evenodd"
+      d="M19.792 6.209c.86.23 1.54.911 1.771 1.771.424 1.566.437 4.814.437 4.814s0 3.26-.424 4.814a2.519 2.519 0 0 1-1.771 1.771c-1.553.424-7.805.424-7.805.424s-6.252 0-7.805-.424a2.519 2.519 0 0 1-1.771-1.771C2 16.042 2 12.794 2 12.794s0-3.248.41-4.8c.232-.861.912-1.541 1.772-1.772 1.554-.424 7.805-.437 7.805-.437s6.252 0 7.805.424Zm-4.608 6.585-5.187 3.004V9.79l5.187 3.004Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+export default FooterYoutubeIcon
